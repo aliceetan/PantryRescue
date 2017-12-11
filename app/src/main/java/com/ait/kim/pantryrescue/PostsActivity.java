a674f4b2505df1487dd2961ff8f46f3f77f3d5c8
Alice Tan <alice.tan@pomona.edu>
2017-12-11 12:02:00 +0100

    Created Posts activity class and adapter

---
+++ app/src/main/java/com/ait/kim/pantryrescue/PostsActivity.java
@@ -0,0 +1,13 @@
+package com.ait.kim.pantryrescue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+    }
+}
---
+++ app/src/main/java/com/ait/kim/pantryrescue/adapter/PostsAdapter.java
@@ -0,0 +1,8 @@
+package com.ait.kim.pantryrescue.adapter;
+
+/**
+ * Created by alicetan on 12/11/17.
+ */
+
+public class PostsAdapter {
+}
